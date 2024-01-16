ac2f1b60b7eb3981d2141a73bf2ea8acfb75f09f
Alper Canimoglu <teknoaydin24@gmail.com>
2024-01-16 22:47:35 +0300

    Birth
    Project was born.

---
+++ NetflixClone-MVVM/NetflixClone-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixClone-MVVM
+//
+//  Created by Alper Canımoğlu on 16.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
